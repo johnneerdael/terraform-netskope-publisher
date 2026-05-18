ec98a8a48d345f817dd4991b9dc08c6860245cc0
John Neerdael <jneerdael@netskope.com>
2026-05-18 19:31:08 +0200

    feat!: remove multi-platform root module; submodules are now the public interface

--- locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  publisher_names = var.names != null ? var.names : [
-    for i in range(var.replicas) :
-    format("%s-%d", var.name_prefix, i + 1)
-  ]
-}
--- versions.tf
+++ versions.tf
@@ -1,14 +1,14 @@
+# This repository is NOT a Terraform module on its own. Source one of the
+# platform submodules instead:
+#
+#   module "publisher" {
+#     source = "github.com/johnneerdael/terraform-netskope-publisher//modules/aws?ref=v2.0.0"
+#     # ...
+#   }
+#
+# Supported platforms: aws | azure | gcp | vsphere.
+# See https://johnneerdael.github.io/terraform-netskope-publisher/ for guides.
+
 terraform {
   required_version = ">= 1.7"
-
-  required_providers {
-    http = {
-      source  = "hashicorp/http"
-      version = ">= 3.4"
-    }
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = ">= 2.3"
-    }
-  }
 }
